0fbfe752d6dc960081d43f878e495ec0b636d526
piotrwita <piotr.witaszewski@gmai.com>
2022-01-06 20:31:29 +0100

    Szyfrowanie RSA

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using RsaExample;
+
+RsaEncryption rsa = new RsaEncryption();
+string cypher = string.Empty;
+
+Console.WriteLine("Enter your text to encrypt");
+var text = Console.ReadLine();  
+if(!string.IsNullOrEmpty(text))
+{
+    cypher = rsa.Encrypt(text);
+    Console.WriteLine($"Encrypted Text: {cypher}");
+}
+
+Console.WriteLine("Press any key to decrypt text");
+Console.ReadLine();
+
+var plainText = rsa.Decrypt(cypher);
+Console.ReadLine();
---
+++ RsaEncryption.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace RsaExample
+{
+    public class RsaEncryption
+    {
+        private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        public RsaEncryption()
+        {
+            _privateKey = csp.ExportParameters(true);
+            _publicKey = csp.ExportParameters(false);
+        }
+
+        public string GetPublicKey()
+        {
+            var sw = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, _publicKey);
+            return sw.ToString();
+        }
+
+        public string Encrypt(string plainText)
+        {
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(_publicKey);
+            var data = Encoding.UTF8.GetBytes(plainText);
+            var cypher = csp.Encrypt(data, false);
+            return Convert.ToBase64String(cypher);
+        }
+
+        public string Decrypt(string cypherText)
+        {
+            var dataBytes = Convert.FromBase64String(cypherText);
+            csp.ImportParameters(_privateKey);
+            var plainText = csp.Decrypt(dataBytes, false);
+            return Encoding.Unicode.GetString(plainText);
+        }
+    }
+}
